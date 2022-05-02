3e0281424c862a575b62a83cbdda28ecad02fd09
AshokKoda <a.koda59@gmail.com>
2022-05-02 22:34:35 +0530

    Added:Welcome message to display

---
+++ README.md
@@ -0,0 +1,1 @@
+# Employee Payroll Service With Java&MySQL
---
+++ src/main/java/controller/EmployeePayroll.java
@@ -0,0 +1,10 @@
+package controller;
+
+public class EmployeePayroll {
+
+	public static void main(String[] args) {
+		System.out.println("************** Welcome to Employee Payroll Service (MySQL) **************");
+
+	}
+
+}
