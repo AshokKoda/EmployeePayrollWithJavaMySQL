378b08c98c20b39045aa1365755beb22d9df3002
AshokKoda <a.koda59@gmail.com>
2022-05-02 22:38:34 +0530

    Added:UC1 - Connect database with java

---
+++ src/main/java/common/Constants.java
@@ -0,0 +1,9 @@
+package common;
+
+public class Constants {
+
+	/* MySql Connection configuration */
+	public static final String JDBC_STR = "jdbc:mysql://localhost:3306/payroll_service";
+	public static final String USERNAME = "root";
+	public static final String PASSWORD = "Koda@1996";
+}
--- src/main/java/controller/EmployeePayroll.java
+++ src/main/java/controller/EmployeePayroll.java
@@ -1,9 +1,15 @@
 package controller;
 
+import dbconnection.PayrollDbService;
+
 public class EmployeePayroll {
 
 	public static void main(String[] args) {
 		System.out.println("************** Welcome to Employee Payroll Service (MySQL) **************");
+
+		PayrollDbService payrollDbService = new PayrollDbService();
+		System.out.println("Please wait Database is connecting......");
+		payrollDbService.getConnection();
 
 	}
 
